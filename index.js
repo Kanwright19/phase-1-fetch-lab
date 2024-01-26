8dec0387a6034ed56362b9afbc442743f1c46d2c
Kairi Wright <kanwright19@gmail.com>
2024-01-26 08:12:05 -0500

    fetch lab complete

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => {
+        console.log(resp); 
+        return resp.json();
+      })
+    .then(books => renderBooks(books))
+    .catch(error => {
+      console.error('Error:', error);
+    })
+  };
   
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if(!response.ok) {
+        throw new Error (response.statusText)
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderBooks(data)
+    })
+    .catch(error => {
+      console.error('Error fetching books:' , error);
+    });
 }
 
 function renderBooks(books) {
@@ -15,3 +39,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+module.exports ={fetchBooks , renderBooks}; 
